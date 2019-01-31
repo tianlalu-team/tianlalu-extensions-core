cd7d095413ec7f2dab17aca35dd6e48446a713a2
兜小神 <kelely@vip.qq.com>
2019-01-31 12:49:24 +0800

    增加 IEnumerableExtensions， IsNullOrEmpty()，HasDuplicates()

---
+++ src/TianLaLu.Extensions/Collections/Generic/IEnumerableExtensions.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace System.Collections.Generic
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public static class IEnumerableExtensions
+    {
+        [DebuggerStepThrough]
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> list)
+        {
+            if (list == null)
+            {
+                return true;
+            }
+
+            if (!list.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool HasDuplicates<T, TProp>(this IEnumerable<T> list, Func<T, TProp> selector)
+        {
+            var d = new HashSet<TProp>();
+            foreach (var t in list)
+            {
+                if (!d.Add(selector(t)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
